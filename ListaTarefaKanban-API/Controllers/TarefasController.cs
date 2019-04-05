8fd1cb65844bd7ee5cab80cff71d8b5870927ee9
Leonardo Frasson <leonardo.frasson@intranet.optcode.com.br>
2019-04-05 17:54:41 -0300

    First Commit

---
+++ ListaTarefaKanban-API/Controllers/TarefasController.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListaTarefaKanban_Domain;
+using ListaTarefaKanban_Repository;
+using ListaTarefaKanban_Resitory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefaKanban_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly IListaTarefaKanbanRepository _repo;
+
+        public TarefasController(IListaTarefaKanbanRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repo.GetAllTarefaAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco Dados Falhou");
+            }
+
+        }
+
+        // GET
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {            
+            try
+            {
+                var results = await _repo.GetAllTarefaResponsavel(id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco Dados Falhou");
+            }
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> Post(Tarefa model)
+        {
+            try
+            {
+                _repo.Add(model);
+                
+                if(await _repo.SaveChangesAsync()){
+                    return Created($"/api/tarefas/{model.Id}", model);
+                }
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco Dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        // PUT 
+        [HttpPut("{TarefaId}")]
+        public async Task<IActionResult> Put(int TarefaId,Tarefa model)
+        {
+            try
+            {
+                var evento = await _repo.GetAllTarefaResponsavel(TarefaId);
+
+                if(evento == null) return NotFound();
+
+                _repo.Update(model);
+                
+                if(await _repo.SaveChangesAsync()){
+                    return Created($"/api/tarefas/{model.Id}", model);
+                }
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco Dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+        // DELETE 
+        [HttpDelete("{TarefaId}")]
+        public async Task<IActionResult> Delete(int TarefaId)
+        {
+            try
+            {
+                var evento = await _repo.GetAllTarefaResponsavel(TarefaId);
+
+                if(evento == null) return NotFound();
+
+                _repo.Delete(evento);
+                
+                if(await _repo.SaveChangesAsync()){
+                    return Ok();
+                }
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco Dados Falhou");
+            }
+
+            return BadRequest();
+        }
+
+    }
+
+}
---
+++ ListaTarefaKanban-Repository/ListaTarefaKanbanRepository.cs
@@ -0,0 +1,59 @@
+using System.Linq;
+using System.Threading.Tasks;
+using ListaTarefaKanban_Domain;
+using ListaTarefaKanban_Resitory;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefaKanban_Repository
+{
+    public class ListaTarefaKanbanRepository : IListaTarefaKanbanRepository
+    {
+        private readonly KanbanContext _context;
+
+        public ListaTarefaKanbanRepository(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+             _context.Add(entity);
+        }
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        //TAREFAS
+        public async Task<Tarefa[]> GetAllTarefaAsync()
+        {
+            IQueryable<Tarefa> query = _context.Tarefas
+            .Include(c => c.Responsavel);
+
+            query = query.OrderBy(c => c.Responsavel.Id);
+
+            return await query.ToArrayAsync();
+
+        }
+
+        public async Task<Tarefa[]> GetAllTarefaResponsavel(int responsavelId)
+        {
+            IQueryable<Tarefa> query = _context.Tarefas
+            .Include(c => c.Responsavel);
+
+            query = query.OrderBy(c => c.DataRealInicio)
+                        .Where(c => c.UsuarioId == responsavelId);
+
+            return await query.ToArrayAsync();
+        }
+
+    }
+}
